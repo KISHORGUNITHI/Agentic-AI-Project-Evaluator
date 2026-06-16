6024673e3bbefabb1a52de26d349720fe4e9d723
KISHORGUNITHI <kishorgunithi067@gmail.com>
2026-06-16 15:40:34 +0530

    added Better-Auth authentication

--- backend/controllers/authcontroller.ts
+++ backend/controllers/authcontroller.ts
@@ -1,15 +1,31 @@
-import { Request, Response } from "express";
+// import {Request, Response} from "express"
+// import pool from "../db/connection_pool";
 
-export default class AuthController {
-    login(req: Request, res: Response): void {
-        throw new Error("Method not implemented yet");
-    }
+// export const login=async(req:Request,res:Response)=>{
+//     console.log("Entered into Login Feature");
+//     res.json({"message":"Login Route Hit!!!"});
+// };
 
-    registration(req: Request, res: Response): void {
-        throw new Error("Method not implemented yet");
-    }
+// export const register=async(req:Request,res:Response)=>{
+//     console.log("Entered into the Registration Feature");
+//     res.json({"message":"Register Route Hit!!!"});
+// };
 
-    refresh(req: Request, res: Response): void {
-        throw new Error("Method not implemented yet");
-    }
-}
+import "dotenv/config";
+import {betterAuth} from "better-auth"
+import {drizzleAdapter} from "@better-auth/drizzle-adapter";
+import {db} from "../db/aidb"
+import * as schema from "../auth-schema";
+
+export const auth=betterAuth({
+    secret:process.env.BETTER_AUTH_SECRET,
+
+    database:drizzleAdapter(db,{
+        provider:"pg",
+        schema,
+    }),
+
+    emailAndPassword:{
+        enabled:true,
+    },
+}) 
--- backend/routes/login.ts
+++
@@ -1,5 +0,0 @@
-import express from "express"
-
-const auth_app=express()
-
-auth_app.post('/login',);
